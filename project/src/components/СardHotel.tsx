836d9228735ca8cb314a906564b4c3d8966476c2
dk.kokorin <dk.kokorin@jet.su>
2022-02-13 21:28:40 +0300

    Начинаем программировать

---
+++ project/src/components/СardHotel.tsx
@@ -0,0 +1,54 @@
+interface hotels {
+  premium: boolean;
+  img: string;
+  price: string;
+  rating: number;
+  name: string;
+  type: string;
+}
+
+function СardHotel(props: hotels) {
+  function getRating(rating: number): number {
+    return rating * 20;
+  }
+
+  return (
+    <article className="cities__place-card place-card">
+      {props.premium &&
+        <div className="place-card__mark">
+          <span>Premium</span>
+        </div>}
+      <div className="cities__image-wrapper place-card__image-wrapper">
+        <a href="#">
+          <img className="place-card__image" src={props.img} width="260" height="200" alt="Place image" />
+        </a>
+      </div>
+      <div className="place-card__info">
+        <div className="place-card__price-wrapper">
+          <div className="place-card__price">
+            <b className="place-card__price-value">&euro;{props.price}</b>
+            <span className="place-card__price-text">&#47;&nbsp;night</span>
+          </div>
+          <button className="place-card__bookmark-button button" type="button">
+            <svg className="place-card__bookmark-icon" width="18" height="19">
+              <use xlinkHref="#icon-bookmark"></use>
+            </svg>
+            <span className="visually-hidden">In bookmarks</span>
+          </button>
+        </div>
+        <div className="place-card__rating rating">
+          <div className="place-card__stars rating__stars">
+            <span style={{ width: `${getRating(props.rating)}%` }}></span>
+            <span className="visually-hidden">Rating</span>
+          </div>
+        </div>
+        <h2 className="place-card__name">
+          <a href="#">{props.name}</a>
+        </h2>
+        <p className="place-card__type">{props.type}</p>
+      </div>
+    </article>
+  );
+}
+
+export default СardHotel;
