dabbcccbed7315d944e4c532067284c82dc2c6e6
dk.kokorin <dk.kokorin@jet.su>
2022-03-10 17:23:17 +0300

    Больше подробностей (часть 1)

---
+++ project/src/components/UI/Map/Map.tsx
@@ -0,0 +1,45 @@
+import { useRef, useEffect } from 'react';
+import useMap from '../../../hooks/useMap';
+import { Hotel } from '../../../types/hotel';
+import { City } from '../../../types/typeMap';
+import leaflet from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import { URL_MARKER_DEFAULT } from './../../marker';
+
+type MapProps = {
+  hotels: Hotel[];
+  city: City;
+};
+
+export default function Map({ hotels, city }: MapProps) {
+  const mapRef = useRef(null);
+  const map = useMap(mapRef, city);
+
+  const defaultCustomIcon = leaflet.icon({
+    iconUrl: URL_MARKER_DEFAULT,
+    iconSize: [40, 40],
+    iconAnchor: [20, 40],
+  });
+
+  // const currentCustomIcon = leaflet.icon({
+  //   iconUrl: URL_MARKER_CURRENT,
+  //   iconSize: [40, 40],
+  //   iconAnchor: [20, 40],
+  // });
+
+  useEffect(() => {
+    if (map) {
+      hotels.forEach((hotel) => {
+        leaflet
+          .marker({
+            lat: hotel.lat,
+            lng: hotel.lng,
+          }, {
+            icon: defaultCustomIcon,
+          })
+          .addTo(map);
+      });
+    }
+  }, [map]);
+  return <section className="cities__map map" ref={mapRef}></section>;
+}
